52b1a110e70cca4875bea5bfc898b43c2f045c4f
William Beuil <william.beuil@gmail.com>
2023-12-05 13:49:47 +0100

    chore: 2023/4

---
+++ 2023/day4/puzzle1.ts
@@ -0,0 +1,23 @@
+export const getWinningAndPlayerNumbers = (line: string): [number[], number[]] => {
+    const matches = line.match(/Card (?: *\d+): ( *\d+(?: *\d+)*) \| ( *\d+(?: *\d+)*)/);
+    if (!matches) throw new Error(`Invalid line: ${line}`);
+    const winningNumbers = matches[1].match(/\d+/g)?.map(Number) || [];
+    const playerNumbers = matches[2].match(/\d+/g)?.map(Number) || [];
+    return [winningNumbers, playerNumbers];
+};
+
+export const handler = (input: string): number => {
+    return input
+        .split("\n")
+        .map((line) => {
+            let points = 0;
+            const [winningNumbers, playerNumbers] = getWinningAndPlayerNumbers(line);
+            for (const playerNumber of playerNumbers) {
+                if (winningNumbers.includes(playerNumber)) {
+                    points = !points ? 1 : points * 2;
+                }
+            }
+            return points;
+        })
+        .reduce((acc, cur) => acc + cur, 0);
+};
---
+++ 2023/day4/puzzle2.ts
@@ -0,0 +1,22 @@
+import { getWinningAndPlayerNumbers } from "./puzzle1";
+
+export const handler = (input: string): number => {
+    const cards: Record<number, number> = {};
+    return input
+        .split("\n")
+        .map((line, i) => {
+            cards[i + 1] = cards[i + 1] ? cards[i + 1] : 1;
+            let copies = 0;
+            const [winningNumbers, playerNumbers] = getWinningAndPlayerNumbers(line);
+            for (const playerNumber of playerNumbers) {
+                if (winningNumbers.includes(playerNumber)) {
+                    copies++;
+                }
+            }
+            for (let j = copies; j > 0; j--) {
+                cards[i + j + 1] = cards[i + j + 1] ? cards[i + j + 1] + cards[i + 1] : 1 + cards[i + 1];
+            }
+            return cards[i + 1];
+        })
+        .reduce((acc, cur) => acc + cur, 0);
+};
