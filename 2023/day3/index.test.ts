1550b9fdb9dfe0cb29d451a0bb109b2913335173
William Beuil <william.beuil@gmail.com>
2023-12-05 12:10:10 +0100

    chore: 2023/3

---
+++ 2023/day3/index.test.ts
@@ -0,0 +1,38 @@
+import { describe, expect, test } from "bun:test";
+
+import { sanitizeInput } from "../../utils";
+import { handler as p1 } from "./puzzle1";
+import { handler as p2 } from "./puzzle2";
+
+describe("day1", () => {
+    test("puzzle1", () => {
+        const input = `
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+`
+        expect(p1(sanitizeInput(input))).toBe(4361);
+    });
+    test("puzzle2", () => {
+        const input = `
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+`
+        expect(p2(sanitizeInput(input))).toBe(467835);
+    });
+});
---
+++ 2023/day3/puzzle2.ts
@@ -0,0 +1,19 @@
+import { findCoordinates, isAdjacent } from './puzzle1'
+
+export const handler = (input: string): number => {
+    const candidates: number[] = []
+    const [numbers, symbols] = findCoordinates(input)
+    for (const symbol of symbols) {
+        if (symbol.char !== '*') continue
+        const adjacentNumbers = []
+        for (const number of numbers) {
+            if (isAdjacent(number, symbol)) {
+                adjacentNumbers.push(number.int)
+            }
+        }
+        if (adjacentNumbers.length === 2) {
+            candidates.push(adjacentNumbers[0] * adjacentNumbers[1])
+        }
+    }
+    return candidates.reduce((acc, cur) => acc + cur, 0)
+}
